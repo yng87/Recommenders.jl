c06ac7799da646f2fc166e7b544c5ceaf155aa56
yng87 <k.yanagi07@gmail.com>
2021-01-28 22:48:06 +0900

    fix

--- src/Dataset.jl
+++ src/Dataset.jl
@@ -55,7 +55,10 @@ end
 
 function loadcsv(path::AbstractString; delim=",", header=0, columns=nothing)
     df = DataFrame(CSV.File(path, delim=delim, header=header))
-    return rename!(df, columns)
+    if !isnothing(columns)
+        rename!(df, columns)
+    end
+    return df
 end
 
 function load_movielens1m(dirpath::AbstractString)
