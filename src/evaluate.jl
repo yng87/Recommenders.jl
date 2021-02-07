ae180869039eda495eebd5f6f09178a14d1ce188
yng87 <yanagi@hep-th.phys.s.u-tokyo.ac.jp>
2021-02-07 22:11:40 +0900

    fix doctest (#7)

--- src/evaluate.jl
+++ src/evaluate.jl
@@ -6,10 +6,15 @@ Compute mean HR.
 - recommends: recommends[i] is list of predictions for i-th input.
 - ys: ys[i] is ground-truth for i-th input.
 
+```@meta
+DocTestSetup = quote
+    using Recommender
+end
+```
+
 ```jldoctest
-using Recommender
-julia> hitrate([[1, 3], nothing, [2, 4]], [1, 2, 3])
-0.3333333333333
+julia> hitrate([[1, 3], nothing, [2, 4], []], [1, 2, 3, 4])
+0.25
 ```
 """
 function hitrate(recommends, ys)
