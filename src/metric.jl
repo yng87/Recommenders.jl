944636efe5419f6e91ca2a71221cc94fd3ea10d7
yng87 <k.yanagi07@gmail.com>
2021-08-26 23:04:50 +0900

    refactoring

--- src/metric.jl
+++ src/metric.jl
@@ -1,68 +1,186 @@
 abstract type AbstractMetric end
 
-struct RecallAtK <: AbstractMetric
-    k::Int64
+abstract type SingleListMetric <: AbstractMetric end
+
+abstract type TopKMetric <: SingleListMetric end
+
+struct Recall <: TopKMetric
+    k::Int
 end
 
-function (recall::RecallAtK)(recommends, ys)
-    rec = 0
-    for (pred, gt) in zip(recommends, ys)
-        if pred === nothing || length(pred) == 0
-            continue
-        end
-        k = min(recall.k, length(pred))
-        pred = pred[1:k]
-        num_hits = length(intersect(pred, gt))
-        rec += num_hits / length(gt)
+function (recall::Recall)(recommend_list, ground_truth)
+    if recommend_list === nothing || length(recommend_list) == 0
+        return 0.0
     end
-    return rec / length(ys)
+
+    k = min(recall.k, length(recommend_list))
+    truncated_recom = recommend_list[1:k]
+    num_hits = length(intersect(truncated_recom, ground_truth))
+    return num_hits / length(ground_truth)
 end
 
-struct PrecisionAtK <: AbstractMetric
-    k::Int64
+struct Precision <: TopKMetric
+    k::Int
 end
 
-function (precision::PrecisionAtK)(recommends, ys)
-    prec = 0
-    for (pred, gt) in zip(recommends, ys)
-        if pred === nothing || length(pred) == 0
-            continue
-        end
-        k = min(precision.k, length(pred))
-        pred = pred[1:k]
-        num_hits = length(intersect(pred, gt))
-        prec += num_hits / length(pred)
+function (precision::Precision)(recommend_list, ground_truth)
+    if recommend_list === nothing || length(recommend_list) == 0
+        return 0.0
     end
-    return prec / length(ys)
+
+    k = min(precision.k, length(recommend_list))
+    truncated_recom = recommend_list[1:k]
+    num_hits = length(intersect(truncated_recom, ground_truth))
+    return num_hits / length(truncated_recom)
 end
 
-
-struct NDCG <: AbstractMetric
-    k::Int64
-end
-
-function (precision::NDCG)(recommends, ys)
-    ndcg = 0
-    for (pred, gt) in zip(recommends, ys)
-        if pred === nothing || length(pred) == 0
-            continue
-        end
-        k = min(precision.k, length(pred))
-        pred = pred[1:k]
-        
-        dcg = 0
-        for i in eachindex(pred)
-            if pred[i] in gt
-                dcg += 1.0 / log2(1.0 + i)
-            end
-        end
-
-        idcg=0
-        for i in 1:min(length(pred), length(gt))
-            idcg+= 1.0 / log2(1.0 + i)
-        end
-
-        ndcg += dcg / idcg
+function _dcg(relevances)
+    result = 0.0
+    for i in eachindex(relevances)
+        # result += (2^relevances[i] - 1) / log2(1.0 + i)
+        result += (relevances[i]) / log2(1.0 + i)
     end
-    return ndcg / length(ys)
+    return result
 end
+
+struct DCG <: TopKMetric
+    k::Int
+end
+
+function (dcg::DCG)(recommend_list, ground_truth, true_relevance = nothing)
+    if recommend_list === nothing || length(recommend_list) == 0
+        return 0.0
+    end
+
+    k = min(dcg.k, length(recommend_list))
+    truncated_recom = recommend_list[1:k]
+
+    if true_relevance === nothing
+        true_relevance = ones(length(ground_truth))
+    end
+
+    relevances = zeros(k)
+    for i = 1:k
+        j = findfirst(truncated_recom[i] .== ground_truth)
+        if !(j === nothing)
+            relevances[i] = true_relevance[j]
+        end
+    end
+    return _dcg(relevances)
+end
+
+struct NDCG <: TopKMetric
+    k::Int
+end
+
+function (ndcg::NDCG)(recommend_list, ground_truth, true_relevance = nothing)
+    if recommend_list === nothing || length(recommend_list) == 0
+        return 0.0
+    end
+
+    k = min(ndcg.k, length(recommend_list))
+    truncated_recom = recommend_list[1:k]
+
+    if true_relevance === nothing
+        true_relevance = ones(length(ground_truth))
+    end
+
+    relevances = zeros(k)
+    for i = 1:k
+        j = findfirst(truncated_recom[i] .== ground_truth)
+        if !(j === nothing)
+            relevances[i] = true_relevance[j]
+        end
+    end
+    dcg = _dcg(relevances)
+
+    n = min(length(ground_truth), k)
+    idcg = _dcg(sort(true_relevance, rev = true)[1:n])
+
+    return dcg / idcg
+end
+
+struct MeanMetric{T<:SingleListMetric} <: AbstractMetric
+    base_metric::T
+end
+
+function (metric::MeanMetric)(recommend_lists, ground_truth_list)
+    result = 0.0
+    for (recom, gt) in zip(recommend_lists, ground_truth_list)
+        result += metric.base_metric(recom, gt)
+    end
+    return result / length(recommend_lists)
+end
+
+MeanRecall(k) = MeanMetric(Recall(k))
+MeanPrecision(k) = MeanMetric(Precision(k))
+MeanDCG(k) = MeanMetric(DCG(k))
+MeanNDCG(k) = MeanMetric(NDCG(k))
+
+# old
+# struct RecallAtK <: AbstractMetric
+#     k::Int64
+# end
+
+# function (recall::RecallAtK)(recommends, ys)
+#     rec = 0
+#     for (pred, gt) in zip(recommends, ys)
+#         if pred === nothing || length(pred) == 0
+#             continue
+#         end
+#         k = min(recall.k, length(pred))
+#         pred = pred[1:k]
+#         num_hits = length(intersect(pred, gt))
+#         rec += num_hits / length(gt)
+#     end
+#     return rec / length(ys)
+# end
+
+# struct PrecisionAtK <: AbstractMetric
+#     k::Int64
+# end
+
+# function (precision::PrecisionAtK)(recommends, ys)
+#     prec = 0
+#     for (pred, gt) in zip(recommends, ys)
+#         if pred === nothing || length(pred) == 0
+#             continue
+#         end
+#         k = min(precision.k, length(pred))
+#         pred = pred[1:k]
+#         num_hits = length(intersect(pred, gt))
+#         prec += num_hits / length(pred)
+#     end
+#     return prec / length(ys)
+# end
+
+
+# struct NDCG <: AbstractMetric
+#     k::Int64
+# end
+
+# function (precision::NDCG)(recommends, ys)
+#     ndcg = 0
+#     for (pred, gt) in zip(recommends, ys)
+#         if pred === nothing || length(pred) == 0
+#             continue
+#         end
+#         k = min(precision.k, length(pred))
+#         pred = pred[1:k]
+
+#         dcg = 0
+#         for i in eachindex(pred)
+#             if pred[i] in gt
+#                 dcg += 1.0 / log2(1.0 + i)
+#             end
+#         end
+
+#         idcg = 0
+#         for i = 1:min(length(pred), length(gt))
+#             idcg += 1.0 / log2(1.0 + i)
+#         end
+
+#         ndcg += dcg / idcg
+#     end
+#     return ndcg / length(ys)
+# end
--- test/metric.jl
+++ test/metric.jl
@@ -1,41 +1,67 @@
 using Test
-using Recommender: RecallAtK, PrecisionAtK, NDCG
+using Recommender: Recall, Precision, DCG, NDCG, MeanRecall, MeanPrecision
 
 @testset "Recall and precision" begin
-    recommends = [[1, 2, 3], [4, 5], [], nothing]
+    recall1 = Recall(1)
 
+    recommends = [[1, 2, 3], [4, 5], [], nothing]
     ys = [[1], [2, 5], [4], [5]]
 
-    recall1 = RecallAtK(1)
-    @test recall1(recommends, ys) == 1 / 4
+    expected_recall1 = [1.0, 0.0, 0.0, 0.0]
+    expected_precision1 = [1.0, 0.0, 0.0, 0.0]
+    expected_recall2 = [1.0, 0.5, 0.0, 0.0]
+    expected_precision2 = [0.5, 0.5, 0.0, 0.0]
 
-    recall2 = RecallAtK(2)
-    @test recall2(recommends, ys) == (1 + 0.5) / 4
+    recall1 = Recall(1)
+    recall2 = Recall(2)
+    precision1 = Precision(1)
+    precision2 = Precision(2)
 
-    prec1 = PrecisionAtK(1)
-    @test prec1(recommends, ys) == 1 / 4
+    for i in eachindex(recommends)
+        @test recall1(recommends[i], ys[i]) == expected_recall1[i]
+        @test recall2(recommends[i], ys[i]) == expected_recall2[i]
+        @test precision1(recommends[i], ys[i]) == expected_precision1[i]
+        @test precision2(recommends[i], ys[i]) == expected_precision2[i]
+    end
 
-    prec2 = PrecisionAtK(2)
-    @test prec2(recommends, ys) == (0.5 + 0.5) / 4
+
+    # mean_recall1 = MeanMetric(Recall(1))
+    mean_recall1 = MeanRecall(1)
+    @test mean_recall1(recommends, ys) == 1 / 4
+
+    mean_recall12 = MeanRecall(2)
+    @test mean_recall12(recommends, ys) == (1 + 0.5) / 4
+
+    mean_prec1 = MeanPrecision(1)
+    @test mean_prec1(recommends, ys) == 1 / 4
+
+    mean_prec2 = MeanPrecision(2)
+    @test mean_prec2(recommends, ys) == (0.5 + 0.5) / 4
 
 end
 
-@testset "NDCG" begin
-    ndcg3 = NDCG(3)
-    preds = [[1, 2, 4]]
-    @test ndcg3(preds, [[1]]) == 1.0
-    @test ndcg3(preds, [[2]]) == 1.0 / log2(1.0 + 2)
-    @test ndcg3(preds, [[4]]) == 1.0 / log2(1.0 + 3)
-    @test ndcg3(preds, [[1, 2]]) == 1.0
-    @test ndcg3(preds, [[1, 4]]) ==
-          (1.0 + 1.0 / log2(1.0 + 3)) / (1.0 + 1.0 / log2(1.0 + 2))
-
-    ndcg2 = NDCG(2)
-    preds = [[1, 2, 4], [5, 7, 9]]
-    gts = [[1, 4], [7]]
-    @test ndcg2(preds, gts) == (1.0 / (1.0 + 1.0 / log2(1.0 + 2)) + 1.0 / log2(1.0 + 2)) / 2
-    @test ndcg3(preds, gts) ==
-          (
-        (1.0 + 1.0 / log2(1.0 + 3)) / (1.0 + 1.0 / log2(1.0 + 2)) + 1.0 / log2(1.0 + 2)
-    ) / 2
+@testset "DCG" begin
+    dcg1 = DCG(1)
+    dcg2 = DCG(2)
+    dcg3 = DCG(3)
 end
+
+# @testset "NDCG" begin
+#     ndcg3 = NDCG(3)
+#     preds = [[1, 2, 4]]
+#     @test ndcg3(preds, [[1]]) == 1.0
+#     @test ndcg3(preds, [[2]]) == 1.0 / log2(1.0 + 2)
+#     @test ndcg3(preds, [[4]]) == 1.0 / log2(1.0 + 3)
+#     @test ndcg3(preds, [[1, 2]]) == 1.0
+#     @test ndcg3(preds, [[1, 4]]) ==
+#           (1.0 + 1.0 / log2(1.0 + 3)) / (1.0 + 1.0 / log2(1.0 + 2))
+
+#     ndcg2 = NDCG(2)
+#     preds = [[1, 2, 4], [5, 7, 9]]
+#     gts = [[1, 4], [7]]
+#     @test ndcg2(preds, gts) == (1.0 / (1.0 + 1.0 / log2(1.0 + 2)) + 1.0 / log2(1.0 + 2)) / 2
+#     @test ndcg3(preds, gts) ==
+#           (
+#         (1.0 + 1.0 / log2(1.0 + 3)) / (1.0 + 1.0 / log2(1.0 + 2)) + 1.0 / log2(1.0 + 2)
+#     ) / 2
+# end
