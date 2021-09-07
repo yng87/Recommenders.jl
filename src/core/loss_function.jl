1d5a86ef26c973a9982ee025b971704a140400d5
yng87 <k.yanagi07@gmail.com>
2021-09-07 19:29:28 +0900

    add ElasticNet loss

--- src/core/loss_function.jl
+++ src/core/loss_function.jl
@@ -28,3 +28,38 @@ end
 function grad(loss::BPRLoss, pred)
     return -1 + σ(pred)
 end
+
+struct ElasticNet <: LossFunction
+    α::Real
+    l1_ratio::Real
+end
+
+function (loss::ElasticNet)(X, y, w)
+    n_samples = length(y)
+    α = loss.α
+    l1_ratio = loss.l1_ratio
+    loss = 1/(2*n_samples) * sum((y - X*w).^2) + α*l1_ratio*sum(abs.(w)) + 0.5*α*(1-l1_ratio)*sum(w.^2)
+    return loss
+end
+
+function cd!(loss::ElasticNet, X, y, w)
+    n = length(y)
+    α = loss.α
+    ρ = loss.l1_ratio
+    γ = n*α*ρ
+
+    for j in 1:n
+        denom = sum(X[:, j].^2) + n*α*(1-ρ)
+
+        z = y' * X[:, j] - (X[:, j]' * X * w - X[:, j]' * X[:, j] * w[j])
+        signz = ifelse(z>=0, 1, -1)
+
+        if (signz>0 && z > γ)
+            w[j]=(z-γ)/denom
+        elseif (signz<0 && z<-γ)
+            w[j] = (z+γ)/denom
+        else
+            w[j]=0
+        end
+    end
+end
--- test/core/loss_function.jl
+++ test/core/loss_function.jl
@@ -1,5 +1,5 @@
-using Test
-using Recommenders: σ, Logloss, grad
+using Test, SparseArrays
+using Recommenders: σ, Logloss, ElasticNet, grad, cd!
 
 @testset "Sigmoid." begin
     @test σ(0.0) == 0.5
@@ -17,6 +17,34 @@ end
     @test logloss(3, 0) > 3
     @test logloss(-100, 0) ≈ 0
     @test logloss(0.0, 1) == logloss(0.0, 0)
+end
+
+@testset "ElasticNet" begin
+    elasticnet = ElasticNet(0.2, 0.1)
+    @test elasticnet(rand(2, 2), zeros(2), zeros(2))==0
+    @test elasticnet(sprand(2, 2, 0.5), spzeros(2), spzeros(2))==0
+    X = [1 0; 1 1]
+    y = [1, 0]
+    w = [1, 1]
+    @test elasticnet(X, y, w) == 1.0 + 0.2*0.1*2 + 0.5*0.2*0.9*2
+
+    elasticnet = ElasticNet(0., 0.)
+    y=[1.5]
+    x = 2.0 .* ones(1,1)
+    w = [0.2]
+    cd!(elasticnet, x, y, w)
+    @test w == [0.75]
+    w=[-3.0]
+    cd!(elasticnet, x, y, w)
+    @test w == [0.75]
+
+    elasticnet = ElasticNet(0.5, 0.1)
+    y=[1.,0.]
+    X=[1. 0.;-1. 2.]
+    w=[0.2, -0.4]
+    cd!(elasticnet, X, y, w)
+    # I derived the numbers below by the elementary calculation by hand.
+    @test w ≈ [0.1/2.9, -0.0]
 end
 
 @testset "Grad of Logloss." begin
